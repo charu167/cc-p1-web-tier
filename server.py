702698b85559f1685db1910fb7d1f5d9a89c7e15
Charudatta Potdar <charupot08@gmail.com>
2025-02-21 10:41:03 -0700

    Running file upload task on a Coroutine

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import threading
+from io import BytesIO
 import boto3
 from flask import Flask, request
 import os
@@ -5,26 +7,28 @@ from dotenv import load_dotenv
 
 
 
+
 load_dotenv()
 aws_access_key_id = os.getenv('aws_access_key_id')
 aws_secret_access_key = os.getenv('aws_secret_access_key')
 region_name = os.getenv('region_name')
-
-
-def uploadFileObj(file_obj, bucket_name, s3_file_key):
-    s3 = boto3.client('s3', 
+s3 = boto3.client('s3', 
                     aws_access_key_id=aws_access_key_id, 
                     aws_secret_access_key=aws_secret_access_key,
                     region_name=region_name)
-    
-    s3.upload_fileobj(file_obj, bucket_name, s3_file_key)
-    
-def get_image_class(image_file_name: str):
-    sdb_client = boto3.client('sdb', 
+
+sdb_client = boto3.client('sdb', 
                             aws_access_key_id=aws_access_key_id, 
                             aws_secret_access_key=aws_secret_access_key,
                             region_name=region_name)
 
+def uploadFileObj(file_data, bucket_name, s3_file_key):
+    res = s3.put_object(Body=file_data, 
+                        Bucket=bucket_name, 
+                        Key=s3_file_key)
+    print(f"res: ${res}\nSuccessfully uploaded the file to S3")
+    
+def get_image_class(image_file_name: str):
     res = sdb_client.get_attributes(DomainName='1229855265-simpleDB',
                             ItemName=image_file_name)
     
@@ -45,8 +49,15 @@ def home():
         inputFile = request.files['inputFile']  # Access the file using the field name
         bucket_name = '1229855265-in-bucket'
         s3_file_key = inputFile.filename  # Use the filename as the S3 key
-       
-        uploadFileObj(inputFile, bucket_name, s3_file_key)
+        
+        file_data = inputFile.read()
+        # uploadFileObj(inputFile, bucket_name, s3_file_key)
+        s3_upload_thread = threading.Thread(
+            target=uploadFileObj,
+            args=(file_data, bucket_name, s3_file_key)
+        )
+        s3_upload_thread.start()
+        
         res = get_image_class(s3_file_key.split('.')[0])
         
         s = f"{s3_file_key}:{res['Attributes'][0]['Value']}"
